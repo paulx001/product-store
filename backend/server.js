ab2b70742a1ac5418117e93a7e0f65e7bf3326aa
paulx001 <paulxiong12345@gmail.com>
2025-09-17 19:20:44 -0400

    set up neon database connection and initialize wishlists table

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import wishlistRoutes from "./routes/wishlistRoutes.js"
+import { sql } from "./config/db.js"
 
 dotenv.config();
 
@@ -18,6 +19,25 @@ app.use(morgan("dev")) // log the requests
 
 app.use("/api/wishlists", wishlistRoutes)
 
-app.listen(PORT, () => {
-    console.log("Server is running on port: " + PORT)
+async function initDB() {
+    try {
+        await sql`
+            CREATE TABLE IF NOT EXISTS wishlists (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                image VARCHAR(255) NOT NULL,
+                link VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `
+        console.log("Database initialized successfully");
+    } catch (error) {
+        console.log("Error initDB", error);
+    }
+}
+
+initDB().then(() => {
+    app.listen(PORT, () => {
+      console.log("Server is running on port: " + PORT)
+    });
 })
