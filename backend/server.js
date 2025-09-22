87eb3d302553d85f3e97421ded57d137fb1b351e
paulx001 <paulxiong12345@gmail.com>
2025-09-22 14:10:15 -0400

    integrate Arcjet middleware into Express server

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,38 @@ app.use(cors()); // enable cross-origin resource sharing
 app.use(helmet()); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
 app.use(morgan("dev")) // log the requests
 
+// apply arcjet rate-limit to all routes
+
+app.use(async (req, res, next) => {
+ try {
+    const decision = await aj.protect(req, {
+        requested: 1 // specifies taht each request consume 1 token
+    })
+
+    if (decision.isDenied()) {
+        if (decision.reason.isRateLimit()) {
+            res.status(429).json({error: "Too many requests"});
+        } else if (decision.reason.isBot()) {
+            res.status(403).json({error: "Bot access denied"});
+        } else {
+            res.status(403).json({error: "Forbidden"});
+        }
+        return;
+    }
+
+    // check for spoofed bots
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+        res.status(403).json({error: "Spoofed bot detected"});
+        return;
+    }
+
+    next();
+ } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+ }   
+})
+
 app.use("/api/wishlists", wishlistRoutes)
 
 async function initDB() {
