82392798ddae958795735827c0b86e1854853df8
paulx001 <paulxiong12345@gmail.com>
2025-10-15 16:58:53 -0400

    implement full product CRUD UI and improve product store logic

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -4,6 +4,7 @@ import { useProductStore } from "../store/useProductStore";
 
 function ProductCard({ product }) {
   const { deleteProduct } = useProductStore();
+
   return (
     <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
       {/* PRODUCT IMAGE */}
@@ -27,9 +28,8 @@ function ProductCard({ product }) {
           </Link>
 
           <button
-            className="btn btn-sm btn-error  btn-outline"
-            onClick={() => deleteProduct(product.id)}
-          >
+            className="btn btn-sm btn-error btn-outline"
+            onClick={() => deleteProduct(product.id)}>
             <Trash2Icon className="size-4" />
           </button>
         </div>
--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -1,12 +1,45 @@
 import { create } from "zustand";
 import axios from "axios";
+import toast from "react-hot-toast";
+import e from "cors";
 
 const BASE_URL = "http://localhost:3000";
 
 export const useProductStore = create((set, get) => ({
+  // products state
   products: [],
   loading: false,
   error: null,
+  currentProduct: null,
+
+  // form state
+  formData: {
+    name:"",
+    price:"",
+    image:"",
+  },
+
+  setFormData: (formData) => set({ formData }),
+  resetForm: () => set({ formData: { name: "", price: "", image: "" } }),
+
+  addProduct: async (e) => {
+    e.preventDefault();
+    set({ loading: true });
+
+    try {
+      const { formData } = get();
+      await axios.post(`${BASE_URL}/api/products`, formData);
+      await get().fetchProducts();
+      get().resetForm();
+      toast.success("Product added successfully");
+      document.getElementById("add-product-modal").close();
+    } catch (error) {
+        console.log("Error in addProduct function", error)
+        toast.error("Something went wrong");
+    } finally {
+      set({ loading: false });
+    }
+  },
 
   fetchProducts: async () => {
     set({ loading: true });
@@ -19,6 +52,52 @@ export const useProductStore = create((set, get) => ({
 
     } finally {
         set({ loading: false });
+    }
+  },
+
+  deleteProducts: async (id) => {
+    set({ loading: true });
+    try {
+        await axios.delete(`${BASE_URL}/api/products/${id}`);
+        set(prev => ({ products: prev.products.filter(product => product.id !== id) }));
+        toast.success("Product deleted successfully");
+    } catch (error) {
+        console.log("Error in deleteProduct function", error)
+        toast.error("Something went wrong");
+    } finally {
+        set({ loading: false });
+    }
+  },
+
+  fetchProduct: async (id) => {
+    set({ loading: true });
+    try {
+        const response = await axios.get(`${BASE_URL}/api/products/${id}`);
+        set({ 
+          currentProduct: response.data.data,
+          formData: response.data.data, // pre-fill form with current product data
+          error: null,
+         });
+    } catch (error) {
+        console.log("Error in fetchProduct function", error)
+        set({ error: "Something went wrong", currentProduct: null });
+    } finally {
+        set({ loading: false });
+    }
+  },
+
+  updateProduct: async (id) => {
+    set({ loading: true });
+    try {
+      const {formData} = get();
+      const response = await axios.put(`${BASE_URL}/api/products/${id}`, formData);
+      set({ currentProduct: response.data.data });
+      toast.success("Product updated successfully");
+    } catch (error) {
+        console.log("Error in updateProduct function", error)
+        toast.error("Something went wrong");
+    } finally {
+      set({ loading: false });
     }
   },
 
